9ed8edb911ebffac69a7faf25859fee8bdc048ec
ryan <ryanyen2-c@my.cityu.edu.hk>
2021-09-24 15:55:14 +0800

    'update'

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-######################## train YOLOv3 example ########################
 import os
 import argparse
 import ast
@@ -10,7 +9,7 @@ import numpy as np
 import mindspore.nn as nn
 from mindspore import context, Tensor
 from mindspore.communication.management import init
-from mindspore.train.callback import CheckpointConfig, ModelCheckpoint, LossMonitor, TimeMonitor
+from mindspore.train.callback import CheckpointConfig, ModelCheckpoint, LossMonitor
 from mindspore.train import Model
 from mindspore.context import ParallelMode
 from mindspore.train.serialization import load_checkpoint, load_param_into_net
@@ -65,8 +64,7 @@ def main(args_opt):
     dataset = create_yolo_dataset(args_opt.mindrecord_file,
                                   batch_size=args_opt.batch_size, device_num=device_num, rank=rank)
     dataset_size = dataset.get_dataset_size()
-    print('The epoch size: ', dataset_size)
-    print("Create dataset done!")
+    print(f"Dataset Created with size: {dataset_size}")
 
     net = yolov3_resnet18(ConfigYOLOV3ResNet18())
     net = YoloWithLossCell(net, ConfigYOLOV3ResNet18())
@@ -94,8 +92,10 @@ def main(args_opt):
     callback = [LossMonitor(10*dataset_size), ckpoint_cb]
     model = Model(net)
     dataset_sink_mode = cfg.dataset_sink_mode
-    print("Start train YOLOv3, the first epoch will be slower because of the graph compilation.")
+    print("============ Start Training ============\nThe first epoch will be slower because of the graph compilation.")
     model.train(args_opt.epoch_size, dataset, callbacks=callback, dataset_sink_mode=dataset_sink_mode)
+
+
 # ------------yolov3 train -----------------------------
 cfg = edict({
     "distribute": False,
@@ -103,8 +103,8 @@ cfg = edict({
     "device_num": 1,
     "dataset_sink_mode": True,
 
-    "lr": 0.001,
-    "epoch_size": 1,
+    "lr": 0.0001,
+    "epoch_size": 300,
     "batch_size": 32,
     "loss_scale" : 1024,
 
@@ -115,40 +115,22 @@ cfg = edict({
     "save_checkpoint_epochs" :1,
     'keep_checkpoint_max': 1,
 
-    "data_url": 's3://yyq-2/DATA/code/yolov3/mask_detection_500',
-    "train_url": 's3://yyq-2/DATA/code/yolov3/yolov3_out/',
-    "label_url": 'obs://mask-detection-hong-kong-bj4/MSPDC/dataset/label',
-    'metadata_url': '',
+    "data_url": 'obs://msc21-dataset2/dataset/',
+    "label_url": 'obs://msc21-dataset2/dataset/labels.csv',
+    'metadata_url': 'obs://msc21-dataset2/dataset/metadata.csv',
 }) 
 
+
 if __name__ == '__main__':
-
-
     parser = argparse.ArgumentParser(description='Yolo Training')
-    parser.add_argument('--data_url', required=True, default=None, help='Location of data.')   
-    parser.add_argument('--train_url', required=True, default=None, help='Location of training outputs.')
-    parser.add_argument('--label_url', required=True, default=None, help='Location of data for labels.')
-    parser.add_argument('--metadata_url', required=True, default=None, help='Location of data for metadata.')
-    parser.add_argument('--dataset_url', required=True, default=None, help='Location of data for images.')
-
+    parser.add_argument('--output_url', required=True, default=None, help='Location of training outputs.')
+    
     args_opt = parser.parse_args()
-    args_opt.data_url = args_opt.dataset_url
-
-    if args_opt.data_url[:5] != 'obs:/' and args_opt.data_url[:4] != 's3:/':
-        args_opt.data_url = 'obs:/' + args_opt.data_url
-    cfg.data_url = args_opt.data_url
     
-    if args_opt.train_url[:5] != 'obs:/' and args_opt.train_url[:4] != 's3:/':
-        args_opt.train_url = 'obs:/' + args_opt.train_url
-    cfg.train_url = args_opt.train_url
-
-    if args_opt.label_url[:5] != 'obs:/' and args_opt.label_url[:4] != 's3:/':
-        args_opt.label_url = 'obs:/' + args_opt.label_url
-    cfg.label_url = args_opt.label_url
+    # The parameter: "args_opt.output_url" refers to the local location of the job training instance.
+    # All the output can save to here, and it will upload all of the directory to the specified OBS location after the training.
     
-    if args_opt.metadata_url[:5] != 'obs:/' and args_opt.metadata_url[:4] != 's3:/':
-        args_opt.metadata_url = 'obs:/' + args_opt.metadata_url
-    cfg.metadata_url = args_opt.metadata_url
+    cfg.ckpt_dir = os.path.join(args_opt.output_url, 'ckpt') # copy the ckpt to the output_url (local output location)
 
     if os.path.exists(cfg.ckpt_dir):
         shutil.rmtree(cfg.ckpt_dir)
@@ -162,23 +144,22 @@ if __name__ == '__main__':
     if not os.path.exists(metadata_path):
         mox.file.copy_parallel(src_url=cfg.metadata_url, dst_url=metadata_path)
 
-    mindrecord_dir_train = os.path.join(data_path,'mindrecord/train')
+    mindrecord_dir_train = os.path.join(data_path,'mindrecord')
 
-    print("Start create dataset!")
+    print("Start creating dataset!")
     # It will generate mindrecord file in args_opt.mindrecord_dir,and the file name is yolo.mindrecord.
     prefix = "yolo.mindrecord"
-    cfg.mindrecord_file = os.path.join(mindrecord_dir_train, prefix)
+    cfg.mindrecord_file = os.path.join(mindrecord_dir_train, 'train', prefix)
     if os.path.exists(mindrecord_dir_train):
         print('The mindrecord file had exists!')
     else:
         image_dir = os.path.join(data_path, 'images')
         if not os.path.exists(mindrecord_dir_train):
             os.makedirs(mindrecord_dir_train)
-        print("Create Mindrecord.")
+        print("Start Creating Mindrecord!")
         
         mindrecord_path = Path(mindrecord_dir_train)
         mindrecord_train_path = mindrecord_path / 'train'
-
         mindrecord_test_path = mindrecord_path / 'test'
 
         mindrecord_path.mkdir(parents=True, exist_ok=True)
@@ -186,9 +167,6 @@ if __name__ == '__main__':
         mindrecord_test_path.mkdir(parents=True, exist_ok=True)
         
         data_to_mindrecord_byte_image(Path(image_dir), Path(mindrecord_dir_train), prefix, 1, Path(label_path), Path(metadata_path), train_test_split=0.99)
-        print("Create Mindrecord Done, at {}".format(mindrecord_dir_train))
-        # if you need use mindrecord file next time, you can save them to yours obs.
-        #mox.file.copy_parallel(src_url=args_opt.mindrecord_dir_train, dst_url=os.path.join(cfg.data_url,'mindspore/train')
+        print("Mindrecord Created at {}".format(mindrecord_dir_train))
 
     main(cfg)
-    mox.file.copy_parallel(src_url=cfg.ckpt_dir, dst_url=cfg.train_url)
